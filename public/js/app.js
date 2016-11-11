c77cfb68f335c20a679858527f8c34cec5297813
tmahamadou01 <mahamadoutraore1@gmail.com>
2016-11-11 19:30:35 +0100

    create 'add ressources form' + display action to 'btn hasard'

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
     $("#modal").click(function(){
         $('.ui.modal').modal('show');
     });
+
+    $('.select.dropdown')
+        .dropdown()
+    ;
+
+    $("#btn-hasard").click(function(){
+        $( "#dimmer-hasard" ).addClass( "active" );
+    });
 });
