bce34002b5ca2441b0e8e0abdec0df384f23ec91
tmahamadou01 <mahamadoutraore1@gmail.com>
2016-11-20 17:05:27 +0100

    add ressources in DB

--- public/index.php
+++ public/index.php
@@ -15,6 +15,7 @@ require "../app/Container.php";
 //routes
 $app->get('/',\App\Controllers\IndexController::class . ':index');
 $app->get('/rest-of-web',\App\Controllers\RessourcesController::class . ':index')->setName('ressources');
+$app->post('/submit-form',\App\Controllers\RessourcesController::class . ':store')->setName('submit');
 
 
 
