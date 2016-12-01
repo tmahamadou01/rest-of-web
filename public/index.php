c58ae78e27bcfd065ffa2aff8f5c6fdcd02a6bf6
tmahamadou01 <mahamadoutraore1@gmail.com>
2016-12-01 20:09:24 +0100

    update ressources files and controller

--- public/index.php
+++ public/index.php
@@ -16,6 +16,7 @@ require "../app/Container.php";
 $app->get('/',\App\Controllers\IndexController::class . ':index');
 $app->get('/rest-of-web',\App\Controllers\RessourcesController::class . ':index')->setName('ressources');
 $app->post('/submit-form',\App\Controllers\RessourcesController::class . ':store')->setName('submit');
+$app->get('/rest-of-web/category/{name}/{id}',\App\Controllers\RessourcesController::class . ':getRessourcesListOfCategory')->setName('category');
 
 
 
