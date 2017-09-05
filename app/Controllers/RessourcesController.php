f895ccbcd0290b8861ce121d71060ee3561e853c
emak <emilien.ak@gmail.com>
2017-09-05 05:26:40 +0200

    Rename app to Le grimoire des urls
    fixes #2

--- app/Controllers/RessourcesController.php
+++ app/Controllers/RessourcesController.php
@@ -31,7 +31,7 @@ class RessourcesController
     public function store(RequestInterface $request, ResponseInterface $response)
     {
         $dbconnect = new DbConnect();
-        $sql = "INSERT INTO Ressources (link, description, Categories_id, name) VALUES (:link, :description, :Categories_id, :nom)";
+        $sql = "INSERT INTO Ressources (link, description, Categories_id, name) VALUES (:link, :description, :Categories_id, :name)";
         //echo $sql;die();
         try {
             $db = $dbconnect->getConnection();
@@ -40,7 +40,7 @@ class RessourcesController
             $stmt->bindParam("link", $_POST['link']);
             $stmt->bindParam("description", $_POST['description']);
             $stmt->bindParam("Categories_id", $_POST['Categories_id']);
-            $stmt->bindParam("nom", $_POST['name']);
+            $stmt->bindParam("name", $_POST['name']);
 
             $stmt->execute();
             //return $response->withRedirect('/rest-of-web');
@@ -63,7 +63,22 @@ class RessourcesController
         $sth = $db->prepare( 'SELECT * FROM Ressources WHERE Categories_id = '.$category_id);
         $sth->execute();
         $result = $sth->fetchAll();
-        $this->container->view->render($response, 'pages/list.twig', ["ressources" => $result, "name" => $category_name]);
 
+        $sth = $db->prepare( 'SELECT * FROM Categories;' );
+        $sth->execute();
+        $category_list = $sth->fetchAll();
+
+        $match_category_id = function ($category) use ($category_id) {
+            return $category['id'] === $category_id;
+        };
+        $category = array_filter($category_list, $match_category_id);
+        $category_name = reset($category)['name'];
+
+        $this->container->view->render($response, 'pages/list.twig',
+            [
+                "ressources" => $result,
+                "categories" => $category_list,
+                "name" => $category_name
+            ]);
     }
 }
--- public/index.php
+++ public/index.php
@@ -14,9 +14,9 @@ require "../app/Container.php";
 
 //routes
 $app->get('/',\App\Controllers\IndexController::class . ':index');
-$app->get('/rest-of-web',\App\Controllers\RessourcesController::class . ':index')->setName('ressources');
+$app->get('/grimoire-url',\App\Controllers\RessourcesController::class . ':index')->setName('ressources');
 $app->post('/submit-form',\App\Controllers\RessourcesController::class . ':store')->setName('submit');
-$app->get('/rest-of-web/category/{name}/{id}',\App\Controllers\RessourcesController::class . ':getRessourcesListOfCategory')->setName('category');
+$app->get('/grimoire-url/categorie/{name}/{id}',\App\Controllers\RessourcesController::class . ':getRessourcesListOfCategory')->setName('category');
 
 
 
